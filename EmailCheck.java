f88ccc84ef059532ef2b68a96c2c5cd028692029
unknown <somulanagireddy123@gmail.com>
2021-03-18 08:15:30 +0530

    [Nagireddy]: added Email checking condition

---
+++ EmailCheck.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+
+public class EmailCheck {
+
+    public static void main(String[] args) {
+        System.out.println("Email Validation");
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Please enter Email: ");
+        String email = scan.nextLine();
+        System.out.println(email);
+        String expression = "^[a-zA-Z0-9]*";
+        String Email = (Pattern.matches(expression, email)) ? "Successfully Valid" : "Invalid";
+	System.out.println("Email " + Email + " ");
+    }
+}
