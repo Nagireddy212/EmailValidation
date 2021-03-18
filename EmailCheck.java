e464d5c7305aba48435ace27c5b8b8cd8f38d6f7
Ngireddy <somulanagireddy@gmail.com>
2021-03-18 22:13:36 +0530

    [Nagireddy]: added cheking email validation

--- EmailCheck.java
+++ EmailCheck.java
@@ -8,7 +8,7 @@ public class EmailCheck {
         System.out.print("Please Enter Email: ");
         String email = scan.nextLine();
         System.out.println(email);
-        String expression = "^[a-zA-Z0-9]+[@]+[a-zA-Z]+[.]+[a-zA-Z]{2,4}";
+        String expression = "^[a-zA-Z0-9]+[.+_-]*+[a-zA-Z0-9]+[@]+[a-zA-Z]+[.]+[a-zA-Z]{2,4}";
         String result = (Pattern.matches(expression, email)) ? "Successfully Valid" : "Invalid";
         System.out.println("Email " + result + " ");
     }
